6c8f3dec4d235313dd3be536d266e4ea500a8b40
Zero <mexopaypal@gmail.com>
2026-06-17 01:55:08 +0100

    Add visibility check functionality

---
+++ visible check.h
@@ -0,0 +1,10 @@
+namespace offsets {
+    uintptr_t CameraRotation = 0x200;
+    uintptr_t LastRenderTime = 0x7e4;
+}
+
+bool IsPlayerVisible(uintptr_t Mesh) {
+    uintptr_t WorldTimeSeconds = read<double>(LocalPtrs::Gworld + offsets::CameraRotation + 0x10);
+    uintptr_t LastRenderTime = read<float>(Mesh + offsets::LastRenderTime);
+    return WorldTimeSeconds - LastRenderTime <= 0.06f;
+}
